ce386108117e2cf30de08860fd710ea8b76209dc
Carlos Eduardo <carlosmaisumavez@gmail.com>
2024-04-18 23:31:09 -0300

    Aprendendo sintaxy Java

---
+++ meu-primeiro-projeto-java/java-anatomia-de-classes/src/edu/carlos/primeirasemana/boletimEstudantil.java
@@ -0,0 +1,12 @@
+package edu.carlos.primeirasemana;
+public class boletimEstudantil {
+    public static void main(String[] args) {
+        int notaProva = 3;
+
+        if (notaProva < 5) {
+            System.out.print("Reprovado");
+        }else {
+            System.out.print("Aprovado!");
+        }
+    }
+}
