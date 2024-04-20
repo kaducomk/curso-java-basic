1988deffcad948b13711ce3c84d4261dfb5f28ed
Carlos Eduardo <carlosmaisumavez@gmail.com>
2024-04-20 18:48:54 -0300

    Comentários

---
+++ DOCUMENTACAO/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+
+    public static void main(String[] args) {
+    }
+
+    /**
+     * Metodo de soma
+     * @param numero1 Adiciona um numero no sistema
+     * @param numero2 Adiciona outro numero no sistema
+     * @return int retorna uma soma entre o numero1 e numero2
+     */
+    public int soma(int numero1, int numero2) {
+        int x = numero1;
+        int y = numero2;
+        return  x + y;   
+    }
+}
