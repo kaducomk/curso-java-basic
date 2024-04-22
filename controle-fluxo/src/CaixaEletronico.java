ab8bf93b6a8dc05b7e881f388bf78d179a43a2c5
Carlos Eduardo <carlosmaisumavez@gmail.com>
2024-04-21 21:32:07 -0300

    ok

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-import java.util.Locale;
 
 public class CaixaEletronico {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- controle-fluxo/src/SistemaMedidas.java
+++ controle-fluxo/src/SistemaMedidas.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 
 public class SistemaMedidas {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
